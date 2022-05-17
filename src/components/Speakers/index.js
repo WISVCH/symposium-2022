64926cbe81356b10305ee9b683bdf6830837ea98
Scott Jochems <scottiej2001@gmail.com>
2022-05-17 15:28:09 +0200

    added a cut off length to the text in speaker descriptions

--- src/components/Speakers/index.js
+++ src/components/Speakers/index.js
@@ -10,14 +10,14 @@ export default () => (
     {speakers.map((speaker, i) => (
       <Row key={i}>
         {i % 2 ? <Column size={6} mSize={4} sSize={12}>
-          <Text> {speaker.abstract} </Text>
+          <Text style={{"-webkit-line-clamp": "10"}}> {speaker.abstract} </Text>
           </Column>
            : null}
         <Column size={6} mSize={8} sSize={12}>
           <Speaker {...speaker} side={i % 2} />          
         </Column>
         {i % 2 ? null : <Column size={6} mSize={4} sSize={12}>
-          <Text> {speaker.abstract}</Text>
+          <Text style={{"-webkit-line-clamp": "9"}}> {speaker.abstract}</Text>
           </Column>}
       </Row>
     ))}
