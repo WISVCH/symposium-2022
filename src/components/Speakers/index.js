c8be868125d30c45a006ef7066b8accc95ac7419
Scott Jochems <scottiej2001@gmail.com>
2022-05-17 13:44:03 +0200

    Allowed speakers abstract next to speaker

--- src/components/Speakers/index.js
+++ src/components/Speakers/index.js
@@ -2,18 +2,23 @@ import React from 'react'
 
 import speakers from './data'
 
-import { Row, Column } from 'components/lib'
+import { Text, Row, Column } from 'components/lib'
 import Speaker from './Speaker'
 
 export default () => (
   <div>
     {speakers.map((speaker, i) => (
       <Row key={i}>
-        {i % 2 ? <Column size={6} mSize={4} sSize={12} /> : null}
+        {i % 2 ? <Column size={6} mSize={4} sSize={12}>
+          <Text> {speaker.abstract} </Text>
+          </Column>
+           : null}
         <Column size={6} mSize={8} sSize={12}>
-          <Speaker {...speaker} side={i % 2} />
+          <Speaker {...speaker} side={i % 2} />          
         </Column>
-        {i % 2 ? null : <Column size={6} mSize={4} sSize={12} />}
+        {i % 2 ? null : <Column size={6} mSize={4} sSize={12}>
+          <Text> {speaker.abstract}</Text>
+          </Column>}
       </Row>
     ))}
   </div>
