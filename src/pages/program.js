0a8898690fb4719734b572885b355dc72af01d6c
sajochems <scottiej2001@gmail.com>
2022-05-25 13:23:09 +0200

    removed facebook link

--- src/pages/program.js
+++ src/pages/program.js
@@ -8,9 +8,6 @@ export default () => (
   <Page>
     <Section>
       <H1>Program</H1>
-      <Text>The program will be revealed here as well as on Facebook. Follow our <Link target="_blank" href="https://www.facebook.com/events/165194104170772/" rel="noopener noreferrer">Facebook page</Link> or check back here to stay up to date.</Text>
-    </Section>
-    <Section>
       <Program />
     </Section>
   </Page>
