1a2a8cec67bed8106244e3b39b5eb26b8d98fdfd
Scott Jochems <scottiej2001@gmail.com>
2022-05-31 20:14:02 +0200

    Logo and title changes

--- src/components/Navigation/Logo.js
+++ src/components/Navigation/Logo.js
@@ -1,11 +1,11 @@
 import styled from 'styled-components'
 import { media } from 'utilities/styles'
 
-import logo from 'assets/logo/symposium_logo_white.svg'
+import logo from 'assets/logo/sympo_logo_final.svg'
 
 export default styled.img.attrs({
   src: () => logo,
-  alt: () => "Care for the Future - Improving health bit by bit"
+  alt: () => "Solving Crime in No Time - The strength of digital forensics"
 })`
   display: block;
   height: 3em;
