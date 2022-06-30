55a4d154f551fb066e35cea6aa78cb03ca66ace1
sajochems <scottiej2001@gmail.com>
2022-06-30 15:37:46 +0200

    Program text update

--- src/pages/program.js
+++ src/pages/program.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Page, H2, H6 } from 'components/lib'
+import { Page, H2, Text } from 'components/lib'
 import Section from 'components/Section'
 import Program from 'components/Program'
 
@@ -8,7 +8,7 @@ export default () => (
   <Page>
     <Section>
       <H2>Program</H2>
-      <H6>Lunch and drinks included with ticket.</H6>
+      <Text>Our host Gabriella Adèr will open the symposium and introduce the first speaker. For the second speaker you can choose which speaker you want to attend. During the break a delicious lunch will be waiting for you; all drinks and food are included with your ticket. After the lunch you can decide again which speaker you would like to attend. Hereafter you can experience for yourself what experts do for their daily jobs during the workshops. You can enroll for the workshops during the symposium. More information on the workshops will be announced.</Text>
       <Program />
     </Section>
   </Page>
