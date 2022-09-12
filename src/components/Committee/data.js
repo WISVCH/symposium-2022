15970e6c1767cc1773102d8d38808ebc70b72223
Scott Jochems <scottiej2001@gmail.com>
2022-09-12 14:23:35 +0200

    more images compressed and files renamed

--- src/components/Committee/data.js
+++ src/components/Committee/data.js
@@ -1,11 +1,11 @@
-import Scott from 'assets/committee/Scott_comp.jpg'
-import Waded from 'assets/committee/Waded_comp.jpg'
-import Thomas from 'assets/committee/Thomas_comp.jpg'
-import Jolijn from 'assets/committee/Jolijn_comp.jpg'
-import Darwin from 'assets/committee/Darwin_comp.jpg'
-import Marjolein from 'assets/committee/Marjolein_comp.jpg'
-import Merel from 'assets/committee/Merel_comp.jpg'
-import Amanda from 'assets/committee/Amanda_comp.jpg'
+import Scott from 'assets/committee/Scott.jpg'
+import Waded from 'assets/committee/Waded.jpg'
+import Thomas from 'assets/committee/Thomas.jpg'
+import Jolijn from 'assets/committee/Jolijn.jpg'
+import Darwin from 'assets/committee/Darwin.jpg'
+import Marjolein from 'assets/committee/Marjolein.jpg'
+import Merel from 'assets/committee/Merel.jpg'
+import Amanda from 'assets/committee/Amanda.jpg'
 
 export default [
   {
--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -5,7 +5,7 @@ import { theme, media, darken } from 'utilities/styles'
 import { H2, Text } from 'components/lib'
 import Texture from 'components/Texture'
 
-import Scott from 'assets/committee/Scott_cutout_comp.png'
+import Scott from 'assets/committee/Scott_cutout.png'
 
 const IntroTexture = Texture.extend`
   ${media.not.medium`
